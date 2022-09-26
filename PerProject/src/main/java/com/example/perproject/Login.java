d841aa207ea7bb813aa717d0b5e1aa1ff71c5247
sung_kyung13 <hhaarrii213@gmail.com>
2022-09-26 16:36:22 +0900

    9.26

--- PerProject/src/main/java/com/example/perproject/Login.java
+++ PerProject/src/main/java/com/example/perproject/Login.java
@@ -1,7 +1,53 @@
 package com.example.perproject;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
-public class Login{
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
+public class Login implements Initializable{
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    @FXML
+    private Button changeBtn;
+    public void changeScene() {
+        try {
+            Parent nextScene
+                    = FXMLLoader.load(getClass().getResource("Main.fxml"));
+            Scene scene = new Scene(nextScene);
+            Stage primaryStage = (Stage) changeBtn.getScene().getWindow();
+            primaryStage.setScene(scene);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private Label changeBtn2;
+    public void changeScene2() {
+        try {
+            Parent nextScene
+                    = FXMLLoader.load(getClass().getResource("SignUp.fxml"));
+            Scene scene = new Scene(nextScene);
+            Stage primaryStage = (Stage) changeBtn2.getScene().getWindow();
+            primaryStage.setScene(scene);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
--- PerProject/src/main/java/com/example/perproject/SignUp.java
+++ PerProject/src/main/java/com/example/perproject/SignUp.java
@@ -1,4 +1,45 @@
 package com.example.perproject;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 public class SignUp {
+
+    @FXML
+    private Button changeBtn3;
+    public void change3Scene() {
+        try {
+            Parent nextScene
+                    = FXMLLoader.load(getClass().getResource("SignUpFini.fxml"));
+            Scene scene = new Scene(nextScene);
+            Stage primaryStage = (Stage) changeBtn3.getScene().getWindow();
+            primaryStage.setScene(scene);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private Label changeBtn4;
+    public void change4Scene() {
+        try {
+            Parent nextScene
+                    = FXMLLoader.load(getClass().getResource("Login.fxml"));
+            Scene scene = new Scene(nextScene);
+            Stage primaryStage = (Stage) changeBtn4.getScene().getWindow();
+            primaryStage.setScene(scene);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
