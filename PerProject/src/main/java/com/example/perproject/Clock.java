404541172c0ba0b64fe16a8dcac4095ddd46db45
sung_kyung13 <hhaarrii213@gmail.com>
2022-09-26 16:17:46 +0900

    9.26

--- PerProject/src/main/java/com/example/perproject/Clock.java
+++ PerProject/src/main/java/com/example/perproject/Clock.java
@@ -6,12 +6,6 @@ import javafx.fxml.Initializable;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class Clock implements Initializable {
+public class Clock{
 
-    @FXML
-    private
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-
-    }
 }
---
+++ PerProject/src/main/java/com/example/perproject/Login.java
@@ -0,0 +1,7 @@
+package com.example.perproject;
+
+import javafx.fxml.Initializable;
+
+public class Login{
+
+}
