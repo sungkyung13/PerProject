b60203677e558f1cd98fa60a11c1beda1be91cd9
sungkyung13 <hhaarrii213@gmail.com>
2022-10-17 15:01:41 +0900

    test

--- PerProject/src/main/java/com/example/perproject/HelloApplication.java
+++ PerProject/src/main/java/com/example/perproject/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Start.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 900);
-        stage.setTitle("HARANG");
+        stage.setTitle("ARANG");
         stage.setScene(scene);
         stage.show();
     }
